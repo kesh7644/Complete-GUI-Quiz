70dac37c26e000e3e72ee9f75a55f8f615753980
Keshava Prabhu <81661005+kesh7644@users.noreply.github.com>
2022-07-14 04:13:05 +0000

    i added a new background image and because my program was looking dull in the previous version

--- main.py
+++ main.py
@@ -1,85 +1,100 @@
-from tkinter import*
+from tkinter import *
 import random
+from PIL import Image, ImageTk
+
 names = []
 global questions_answers
 asked = []
 
 questions_answers = { #Questions that will be asked
-     1: ["Who won the 2002 FIFA World cup?", 'Brazil', 'America','UK', 'Austrailia' ,'Brazil',1],
-    2: ["Which NBA player has won the most championships?",'Michael Jordan','Bill Russel','Lebron James', 'Wilt Chamberlain','Bill Russel',2],
-    3: ["Which position in soccer is allowed to touch the ball?", 'Forward','Midfielder', 'Defender','Goalkeeper','Goalkeeper',4],
-}
+    1: ["For how many days is a Test match scheduled?", '100 overs', '3 days','One day ', '5 days' ,'5 days',4],
+    2: [" The Olympics are held every how many years?",'3 years','8 years','4 years', '1 year','4 years',3],
+    3: ["In soccer, what body part can’t touch the ball?", 'Head','Hands','Legs','Chest','Hands',2],
+    4: ["What’s the diameter of a basketball hoop in inches?",'14 Inches','16 Inches','18 Inches','20 Inches','18 Inches',3],
+    5: ["Which batsman started his international cricketing career at the age of 16?",'MS Dhoni','Virat Kohli','Joe Root','Sachin Tendulkar','Sachin Tendulkar',4],
+    6: ["How many players are there in a football (soccer) team?",'7','9','11','13','11',3],
+    7: ["Which sport is not played with a ball?",'Basketball','Football','Cricket','Ice Hockey','Ice Hockey',4],
+    8: ["Where will the 2023 Cricket World Cup be hosted?",'Australia','India','New Zealand','England','India',2],
+    9: ["Which famous boxer is frequently ranked as the best heavyweight boxer of all time?",'Muhammad Ali','Tyson Fury','Mike Tyson','Anthony Joshua','Muhammad Ali',1],
+    10: ["Which sport does Serena Williams play?",'Basketball','Tennis','Cricket','Football','Tennis',2],
 
+
+
+}
 def randomiser():
     global qnum
-    qnum = random.randint(1,3)
+    qnum = random.randint(1,10)
     if qnum not in asked:
       asked.append(qnum)
     elif qnum in asked:
       randomiser()
-   
+     
 
 class QuizStarter:
   def __init__(self, parent):
-    background_color="lightgreen"
-   
-    self.quiz_frame = Frame(parent, bg = background_color, padx=100, pady=100)
-    self.quiz_frame.grid()
+    background_color="lightgrey"
 
-    self.heading_label=Label(self.quiz_frame, text = "Sports Quiz", font =( "Tw Cen MT","18","bold"))
-    self.heading_label.grid(row= 0, padx=20)
+    self.heading_label=Label(window, text = "Car quiz", font =( "Times","25","bold"),bg=background_color)
+    self.heading_label.place(x=520, y=100)
 
     self.var1=IntVar()
 
-    self.user_label=Label(self.quiz_frame, text="Please Enter your Username Below: ", font=( "Tw Cen MT","18","bold"),bg=background_color)
-    self.user_label.grid(row=1, padx=20, pady=20)
+    self.user_label=Label(window, text="Please Enter your Username Below: ", font=( "Times","18","bold"),bg=background_color)
+    self.user_label.place(x=450, y=300)
 
-    self.entry_box=Entry(self.quiz_frame)
-    self.entry_box.grid(row=2,padx=20, pady=20)
+    self.entry_box=Entry(window)
+    self.entry_box.place(x=600, y=400)
 
-    self.continue_button = Button(self.quiz_frame, text="Continue", font=( "Helvetica","13","bold"), bg="darkgrey",command=self.name_collection)
-    self.continue_button.grid(row=3,padx=20, pady=20)
+    self.start_button = Button(window, text="START", font=( "Helvetica","13","bold"), bg="whitesmoke",command=self.name_collection)
+    self.start_button.place(x=642, y=450)
 
   def name_collection(self):
         name=self.entry_box.get()
         names.append(name)
-        self.quiz_frame.destroy()
-        Quiz(root)
+        self.heading_label.destroy()
+        self.user_label.destroy()
+        self.entry_box.destroy()
+        self.start_button.destroy()
+        Quiz(window)
 
 class Quiz:
 
    def __init__(self, parent):
-    background_color="red"
-   
+    background_color="lightgrey"
+ 
+ 
     self.quiz_frame = Frame(parent, bg = background_color, padx=40, pady=40)
     self.quiz_frame.grid()
 
     randomiser()
 
-    self.question_label=Label(self.quiz_frame, text = questions_answers[qnum][0], font =( "Tw Cen MT","18","bold"))
+    self.question_label=Label(window, text = questions_answers[qnum][0], font =( "Tw Cen MT","18","bold"))
     self.question_label.grid(row= 0, padx=10, pady=10)  
 
     self.var1=IntVar()
 
-    self.rb1 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][1], font=("Helvetica", "12"), bg=background_color, value=1, variable=self.var1, pady=10)
+    self.rb1 = Radiobutton(window, text = questions_answers[qnum][1], font=("Helvetica", "12"), bg=background_color, value=1, variable=self.var1, pady=10)
     self.rb1.grid(row=1, sticky=W)
 
-    self.rb2 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][2], font=("Helvetica", "12"), bg=background_color, value=1, variable=self.var1, pady=10)
+    self.rb2 = Radiobutton(window, text = questions_answers[qnum][2], font=("Helvetica", "12"), bg=background_color, value=2, variable=self.var1, pady=10)
     self.rb2.grid(row=2, sticky=W)
 
-    self.rb3 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][3], font=("Helvetica", "12"), bg=background_color, value=1, variable=self.var1, pady=10)
+    self.rb3 = Radiobutton(window, text = questions_answers[qnum][3], font=("Helvetica", "12"), bg=background_color, value=3, variable=self.var1, pady=10)
     self.rb3.grid(row=3, sticky=W)
 
-    self.rb4 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][4], font=("Helvetica", "12"), bg=background_color, value=1, variable=self.var1, pady=10)
+    self.rb4 = Radiobutton(window, text = questions_answers[qnum][4], font=("Helvetica", "12"), bg=background_color, value=4, variable=self.var1, pady=10)
     self.rb4.grid(row=4, sticky=W)
 
-    self.confirm_button = Button(self.quiz_frame, text="Confrim",bg="white",command=self.test_progress)
+    self.confirm_button = Button(window, text="Confrim",bg="white",command=self.test_progress)
     self.confirm_button.grid(row=6)
+    self.score_label  = Label(window, text =
+                             'score')
+    self.score_label.grid(row= 7)  
      
      
    def questions_setup(self):
      randomiser()
-     self.varl.set(0)
+     self.var1.set(0)
      self.question_label.config(text=questions_answers[qnum][0])
      self.rb1.config(text=questions_answers[qnum][1])
      self.rb2.config(text=questions_answers[qnum][2])
@@ -89,42 +104,47 @@ class Quiz:
  
    def test_progress(self):
       global score
+      score = 0
       scr_label=self.score_label
       choice=self.var1.get()
       if len(asked)>9:
         if choice == questions_answers[qnum][6]:
           score +=1
-          sc_label.configure(text=score)
-          self.confirm_button.config(text-"Confirm")
+          scr_label.configure(text=score)
+          self.confirm_button.config(text="Confirm")
         else:
           score+=0
-          sc_label.configure(text-"The correct answer was: "+ questionsanswers[qnum][5] )
-          self.confirm_button.config(text-"confirm")
+          scr_label.configure(text="The correct answer was: "+ questions_answers[qnum][5] )
+          self.confirm_button.config(text="confirm")
      
       else:
             if choice==0:
-              self.confirm_button.config(text-"Try Again, you didn't select an option then submit again" )
+              self.confirm_button.config(text="Try Again, you didn't select an option then submit again" )
               choice=self.var1.get()
             else:
               if choice == questions_answers[qnum][6]:
                 score+=1
-                sc_label.configure(text=score)
-                self.confrim_button.config(text-"confirm")
+                scr_label.configure(text=score)
+                self.confirm_button.config(text="confirm")
                 self.questions_setup()
  
               else:
                   score+=0
-                  scr_label.configure(text-"The correct answer was: "
-                                      + questions_answers [qnum][5])
-                  self.confrim_button.config(text-"Confirmn")
+                  scr_label.configure(text="The correct answer was: " + questions_answers[qnum][5])
+                  self.confirm_button.config(text="Confirmn")
                   self.questions_setup()
-
-
-
-
        
-if __name__ == "__main__":
-  root = Tk()
-  root.title("Sports Quiz")
-  quiz_instance = QuizStarter(root)
-  root.mainloop()
+
+
+if __name__== "__main__":
+    window = Tk()
+    window.title("12CSC Quiz")
+    window.geometry("600x600")
+    bg_image = Image.open("img4.jpg")
+    bg_image = bg_image.resize((1000,600),Image.ANTIALIAS)
+    bg_image = ImageTk.PhotoImage(bg_image)
+    image_label= Label(window, image=bg_image)
+    image_label.place(x=0, y=0, relwidth=1, relheight=1)
+    start_object = QuizStarter(window)
+
+    window.mainloop()
